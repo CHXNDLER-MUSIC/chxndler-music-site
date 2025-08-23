e2aab1a42d39f469e0247cd50417c99fe2626063
CHXNDLER-MUSIC <info@chxndler-music.com>
2025-08-23 02:25:12 -0400

    Create JoinAliensBox.tsx

---
+++ components/JoinAliensBox.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import JoinAliens from "@/components/JoinAliens";
+
+export default function JoinAliensBox() {
+  return (
+    <div className="h-full w-full rounded-2xl bg-black/35 backdrop-blur-md p-3 glow cockpit-glow">
+      <JoinAliens />
+    </div>
+  );
+}
